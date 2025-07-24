3a2868c8061a6a869beebfe14a2942b5d08ab683
Abhinandan2424 <abhipowar3030@gmail.com>
2025-07-24 12:01:46 +0530

    update all files

--- 01_hello_world/app.py
+++ 01_hello_world/app.py
@@ -1,26 +1,11 @@
-from flask import Flask ,request
+from flask import Flask 
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    return "Hey Abhi! this new Flask Application"
-
-@app.route('/about')
-def about():
-    return "This is about us!"
-
-@app.route("/contact")
-def contact():
-    return "This is contact page!"
-
-@app.route("/submit", methods =['GET','POST'])
-def submit():
-    if request.method == 'POST':
-        return 'You send data'
-    else:
-        return 'You are only viewing the form'
+    return "hello World!"
 
 
 if __name__ == "__main__":
